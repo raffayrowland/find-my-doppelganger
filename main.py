a722300874867eec89be30d96ddfdf34f2c727be
Raffay <redruffraff@gmail.com>
2026-01-27 21:04:05 +0000

    minor tweaks and moving things around

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PIL import Image
 
 populate_database()
 
-nearest = get_nearest_neighbors_cosine(make_embedding("example.jpeg"))
-print(get_nearest_neighbors_cosine(make_embedding("example.jpeg")))
+nearest = get_nearest_neighbors_cosine(make_embedding("download.jpeg"))
+print(nearest)
 
 def open_image(path: str) -> Image.Image:
     return Image.open(path)
--- populate_database.py
+++ populate_database.py
@@ -7,9 +7,9 @@ def make_embedding(path):
     embedding = DeepFace.represent(
         img_path=path,
         model_name="Facenet512",
-        detector_backend="opencv",
+        detector_backend="skip",
         align=True,
-        enforce_detection=False
+        enforce_detection=True
     )[0]['embedding']
     return embedding
 
@@ -21,9 +21,10 @@ def populate_database():
     startTime = time.time()
     processed = 0
     for i in range(databaseSize, 70000):
-        processed += 1
-        rate = processed / (time.time() - startTime)
-        print(f"Processing image {i} at rate {rate:.2f}/s.  ETA: {(((70000 - i) / rate) // 60):.2f} minutes")
         embedding = make_embedding(os.path.join("dataset", imgs[i]))
         add_embedding(image_key=os.path.join("dataset", imgs[i]),
                       embedding=embedding)
+        processed += 1
+        rate = processed / (time.time() - startTime)
+        print(f"Processing image {i} at rate {rate:.2f}/s.  ETA: {(((70000 - i) / rate) // 60)} minutes")
+
